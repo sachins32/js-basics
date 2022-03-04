0716995c262e3abc7b21b679de992938573c157f
sachins12 <sachin@predera.com>
2022-03-04 14:22:16 +0530

    rename folder

---
+++ conditional_statements/if_else.js
@@ -0,0 +1,12 @@
+// Greet Based On Hour
+// If hour is between 6am and 12pm: Good Morning!
+// If hour is between 12pm and 6pm: Good Afternoon!
+// Otherwise: Good evening!
+let hour = 10;
+
+if(hour >= 6 && hour < 12) 
+    console.log('Good morning!');
+else if(hour >= 12 && hour < 18)
+    console.log('Good afternoon!');
+else
+    console.log('Good evening!');
