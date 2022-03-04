55babfc0f3c2a4ca0fcd48491c8d1c5beb729cff
sachins12 <sachin@predera.com>
2022-03-04 21:21:07 +0530

    sum of multiples of 3 and 5

---
+++ excercise/sumofmultiples3and5.js
@@ -0,0 +1,16 @@
+// sum(10) 
+// Multiples of 3: 3, 6, 9
+// Multiples of 5: 5, 10
+// Result = 3 + 6 + 9 + 5 + 10 = 33
+
+function sum(limit) {
+    let sum = 0;
+
+    for(let i = 1; i <= limit; i++) 
+        if (i % 3 === 0 || i % 5 == 0) 
+            sum += i;
+            
+    return sum;
+}
+
+console.log(sum(10));
