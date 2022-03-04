0dca0e261129a5f78205564fad0adc32258dbd83
sachins12 <sachin@predera.com>
2022-03-04 11:23:43 +0530

    Primitive types

--- primitive types/index.js
+++ primitive types/index.js
@@ -9,6 +9,6 @@ let name = 'Sachin'; // String literal
 let age = 25; // Number literal
 let isApproved = false; // Boolean Literal
 let firstName = undefined; // undefined is a type and also a value
-let lastName = null; 
+let lastName = null; // default value for object
 
 // In ES6 we have one more type i.e symbol
