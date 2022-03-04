789c0c9de94187adcaefa7b12f12b1bfc5292388
sachins12 <sachin@predera.com>
2022-03-04 11:18:44 +0530

    Primitive types

---
+++ primitive types/index.js
@@ -0,0 +1,14 @@
+// 5 types
+// string
+// number
+// boolean
+// undefined
+// null
+
+let name = 'Sachin'; // String literal
+let age = 25; // Number literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined; // undefined is a type and also a value
+let lastName = null; 
+
+// In ES6 we have one more type i.e symbol
