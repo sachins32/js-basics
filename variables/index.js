7937f26b52cbabe1d234b8065e8d013454a3f5f8
sachins12 <sachin@predera.com>
2022-03-04 11:08:39 +0530

    declaring variables

---
+++ variables/index.js
@@ -0,0 +1,13 @@
+// Best practice to use let to define variables, var has some problems
+
+let firstName;
+console.log(firstName);  // Undefined
+
+let age = 20;
+console.log(age); // 20
+
+// Variables cannot be a reserved keyword
+// cannot start with number
+// name should be meaningful
+// cannot contain a space or hypen
+// Are case sensitive
