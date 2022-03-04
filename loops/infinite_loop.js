14e75ba8e9b8818c97674e9bd954f09bb49551a6
sachins12 <sachin@predera.com>
2022-03-04 15:06:37 +0530

    infinite loops

---
+++ loops/infinite_loop.js
@@ -0,0 +1,24 @@
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    //i++;
+}
+
+while(true) {
+
+}
+
+let x = 0;
+do {
+// x++;
+} while(x < 5);
+
+for(let i = 0; i > 0; i++){
+
+}
+
+for(let i=0; i>0; ) {
+
+}
+
+// Avoid infinite loops
